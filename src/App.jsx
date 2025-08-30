377ad803dac189bba74bdef17caa5b497c958990
Mindblowinggg <akahar36@gmail.com>
2025-08-31 02:58:40 +0530

    as

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import "../App.css"
+import "./App.css"
 
 const App = () => {
   return (
