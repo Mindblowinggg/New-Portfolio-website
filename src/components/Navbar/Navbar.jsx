0d6639a447f3fe030fc1c7c3ef967502542965fe
Mindblowinggg <akahar36@gmail.com>
2025-08-30 14:52:05 +0530

    s

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,24 @@
-
 import React, { useState } from "react";
-import { AnimatePresence } from "framer-motion"; 
+import { AnimatePresence } from "framer-motion";
 import navbarstyles from "../Navbar/Navbar.module.css";
 import MenuBtn from "./MenuBtn";
 import SideMenu from "./SideMenu";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const handleToggle = () => setMenuOpen(!menuOpen);
+
+  const [isToggling, setIsToggling] = useState(false); // New state to prevent rapid clicks
+
+  const handleToggle = () => {
+    if (isToggling) {
+      return;
+    }
+    setIsToggling(true);
+    setMenuOpen(!menuOpen);
+    setTimeout(() => {
+      setIsToggling(false);
+    }, 1000);
+  };
 
   return (
     <nav className={navbarstyles.nav}>
@@ -15,10 +26,7 @@ const Navbar = () => {
         <h1>AK</h1>
       </div>
       <MenuBtn menuOpen={menuOpen} handleToggle={handleToggle} />
-      <AnimatePresence>
-       
-        {menuOpen && <SideMenu />}
-      </AnimatePresence>
+      <AnimatePresence>{menuOpen && <SideMenu />}</AnimatePresence>
     </nav>
   );
 };
