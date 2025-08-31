4ce866fb80338b595f0133da09caf80924914afd
Mindblowinggg <akahar36@gmail.com>
2025-09-01 02:00:48 +0530

    mn

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import styles from '../Home2/Home2.module.css';
-import ParagraphText from './ParagraphText';
+import React from "react";
+import styles from "../Home2/Home2.module.css";
+import ParagraphText from "./ParagraphText";
 
 const Home2 = () => {
   return (
     <div className={styles.Home2container}>
-      <div className={styles.profilepic}>
-      
-      </div>
+      <div className={styles.profilepic}></div>
       <ParagraphText />
+      <button style={{padding:'10px 20px' , height:"50px" , width:"150px" , fontSize:'20px'}}>More about me</button>
     </div>
   );
 };
