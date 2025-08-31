e9a26ed34c6475d0bd5216e052d6c332ba4641cf
Mindblowinggg <akahar36@gmail.com>
2025-08-31 13:39:13 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import "./App.css"
+import "./App.css";
 
 const App = () => {
   return (
     <div className="appcontainer">
       <Navbar />
-      <Home/>
-      <Home/>
-      <Home/>
+      <Home />
+      <Home />
+      <Home />
     </div>
   );
 };
 
 export default App;
-
-
