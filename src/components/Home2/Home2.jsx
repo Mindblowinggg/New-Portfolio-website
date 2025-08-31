24014e9aee60e02dcf99f24361bd6f853235223f
Mindblowinggg <akahar36@gmail.com>
2025-08-31 19:04:53 +0530

    sds

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import styles from '../Home2/Home2.module.css'
+import React from 'react';
+import styles from '../Home2/Home2.module.css';
 import ParagraphText from './ParagraphText';
 
 const Home2 = () => {
   return (
     <div className={styles.Home2container}>
-      <ParagraphText/>
+      <div className={styles.profilepic}>
+      
+      </div>
+      <ParagraphText />
     </div>
-  )
-}
+  );
+};
 
 export default Home2;
