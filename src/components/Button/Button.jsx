cf41cbf5ed7596841fa352cd736753f113ee01d0
Mindblowinggg <akahar36@gmail.com>
2025-09-01 17:42:58 +0530

    sd

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = ({ text = "ALL<br/>PROJECTS" }) => {
+  return (
+    <div className={styles.buttonContainer}>
+      <div className={styles.buttonCircle}></div>
+      
+      <span 
+        className={styles.buttonText}
+        dangerouslySetInnerHTML={{ __html: text }}
+      />
+    </div>
+  );
+};
+
+export default Button;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import TEXTEFFECT from "../TEXTEFFECT/TEXTEFFECT";
 import styles from "../Projects/Projects.module.css";
+import Button from "../Button/Button";
 
 const Projects = () => {
   return (
     <div className={styles.container}>
-      <TEXTEFFECT text="FEATURED WORK" fontSize={"16px"} />
+      <TEXTEFFECT text="FEATURED WORK" fontSize={"20px"} />
       <TEXTEFFECT text={"PROJECTS"} fontSize={"64px"} />
       <p style={{padding:"8px", fontSize:"20px" , fontWeight:"350" , fontFamily:"Poppins" , lineHeight:"25px"}}>
         Please explore my selected projects below . Click on each one for an
         overview
       </p>
+      <Button/>
     </div>
   );
 };
