a6cb8a76c9c4b1e23de83aa907df3c751198c03b
Mindblowinggg <akahar36@gmail.com>
2025-09-02 02:01:34 +0530

    we

--- src/components/Serviceinfocard/ServiceInfoCard.jsx
+++ src/components/Serviceinfocard/ServiceInfoCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaChevronDown } from 'react-icons/fa';
 // eslint-disable-next-line no-unused-vars
 import { AnimatePresence, motion } from 'framer-motion';
-import styles from '../Serviceinfocard/Serviceinfocard.module.css';
+import styles from '../ServiceinfoCard/ServiceinfoCard.module.css';
 
 const ServiceInfoCard = ({ title, description, index, isOpen, onCardClick, className }) => {
   const formattedIndex = (index + 1).toString().padStart(2, "0");
@@ -23,6 +23,7 @@ const ServiceInfoCard = ({ title, description, index, isOpen, onCardClick, class
           </motion.div>
         </div>
       </div>
+
       <AnimatePresence>
         {isOpen && (
           <motion.div
