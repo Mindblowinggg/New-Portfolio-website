596080f87d2df8502f26f18f1176e9ce9b86c506
Mindblowinggg <akahar36@gmail.com>
2025-09-03 15:09:31 +0530

    sd

--- src/assets/ProjectData.js
+++ src/assets/ProjectData.js
@@ -1,47 +1,56 @@
 export const projectsData = [
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-1-800x534.jpg', 
-    heading: 'LILA',
-    category: 'Tortillas',
+    media:
+      "https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-1-800x534.jpg",
+    heading: "LILA",
+    category: "Tortillas",
   },
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-2-800x534.jpg', 
-    heading: 'COFFEE SHOP',
-    category: 'Lifestyle',
+    media:
+      "https://images.unsplash.com/photo-1525610553991-2bede1a236e2?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    heading: "COFFEE SHOP",
+    category: "Lifestyle",
   },
   {
-    media: 'https://demo.themetorium.net/html/jesper/assets/vids/1920/video-4-1920.mp4', 
-    heading: 'FASHION SHOOT', 
-    category: 'Artistic',
+    media:
+      "https://demo.themetorium.net/html/jesper/assets/vids/1920/video-4-1920.mp4",
+    heading: "FASHION SHOOT",
+    category: "Artistic",
   },
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-3-800x534.jpg', 
-    heading: 'BRANDING',
-    category: 'Artistic',
+    media:
+      "https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-3-800x534.jpg",
+    heading: "BRANDING",
+    category: "Artistic",
   },
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-4-800x534.jpg', 
-    heading: 'PRODUCT DESIGN',
-    category: 'Wellness',
+    media:
+      "https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-4-800x534.jpg",
+    heading: "PRODUCT DESIGN",
+    category: "Wellness",
   },
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-5-800x534.jpg', 
-    heading: 'PINK OASIS',
-    category: 'Lifestyle',
+    media:
+      "https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-5-800x534.jpg",
+    heading: "PINK OASIS",
+    category: "Lifestyle",
   },
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-6-800x534.jpg', 
-    heading: 'TURBULENT WATERS',
-    category: 'Lifestyle',
+    media:
+      "https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-6-800x534.jpg",
+    heading: "TURBULENT WATERS",
+    category: "Lifestyle",
   },
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-7-800x534.jpg', 
-    heading: 'BABY CARE',
-    category: 'Wellness',
+    media:
+      "https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-7-800x534.jpg",
+    heading: "BABY CARE",
+    category: "Wellness",
   },
   {
-    media: 'https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-8-800x534.jpg', 
-    heading: 'DREAM SPA',
-    category: 'Wellness',
+    media:
+      "https://webredox.net/demo/wp/jesper/wp-content/uploads/2025/06/portfolio-8-800x534.jpg",
+    heading: "DREAM SPA",
+    category: "Wellness",
   },
 ];
